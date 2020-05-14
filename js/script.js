eea78cf362986e8de5152d319d6e397d7027f94f
Dennis Good <dgood@fusionalliance.com>
2020-05-14 15:29:51 -0400

    Added dynamic text

--- js/script.js
+++ js/script.js
@@ -1,4 +1,80 @@
 function includeHTML() {
+  // Dynamic text
+
+  // get the element
+  const text = document.querySelector('.typing-text');
+
+  // make a words array
+  const words = [
+    "Designer.",
+    "Illustrator.",
+    "Dad.",
+    "Husband."
+  ];
+
+  // start typing effect
+  setTyper(text, words);
+
+  function setTyper(element, words) {
+
+    const LETTER_TYPE_DELAY = 75;
+    const WORD_STAY_DELAY = 2000;
+
+    const DIRECTION_FORWARDS = 0;
+    const DIRECTION_BACKWARDS = 1;
+
+    var direction = DIRECTION_FORWARDS;
+    var wordIndex = 0;
+    var letterIndex = 0;
+
+    var wordTypeInterval;
+
+    startTyping();
+
+    function startTyping() {
+      wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+    }
+
+    function typeLetter() {
+      const word = words[wordIndex];
+
+      if (direction == DIRECTION_FORWARDS) {
+        letterIndex++;
+
+        if (letterIndex == word.length) {
+          direction = DIRECTION_BACKWARDS;
+          clearInterval(wordTypeInterval);
+          setTimeout(startTyping, WORD_STAY_DELAY);
+        }
+
+      } else if (direction == DIRECTION_BACKWARDS) {
+        letterIndex--;
+
+        if (letterIndex == 0) {
+          nextWord();
+        }
+      }
+
+      const textToType = word.substring(0, letterIndex);
+
+      element.textContent = textToType;
+    }
+
+    function nextWord() {
+
+      letterIndex = 0;
+      direction = DIRECTION_FORWARDS;
+      wordIndex++;
+
+      if (wordIndex == words.length) {
+        wordIndex = 0;
+      }
+
+    }
+  }
+
+  // Sample JS
+
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
@@ -9,15 +85,15 @@ function includeHTML() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      } 
+      }
       xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
